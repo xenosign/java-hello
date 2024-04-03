bf9ce351c0e669097721719433125d4cf671f21c
xenosign <xenosign@naver.com>
2024-04-03 16:31:06 +0900

    240403 KB 시범강의 코드풀이

---
+++ src/KB/Car.java
@@ -0,0 +1,30 @@
+package KB;
+
+public class Car {
+    private int gas;
+
+    void setGas(int gas) {
+        this.gas = gas;
+    }
+
+    public boolean isLeftGas() {
+        return gas > 0;
+    }
+
+    void run() {
+        if (isLeftGas()) {
+            System.out.println("gas 가 있습니다.");
+            System.out.println("출발 합니다.");
+
+            while (isLeftGas()) {
+                System.out.println("달립니다. (gas 잔량 : " + gas-- + ")");
+            }
+            
+            System.out.println("멈춥니다. (gas 잔량 : " + gas + ")");
+        } else {
+            System.out.println("Gas 를 주입하세요.");
+        }
+
+
+    }
+}
---
+++ src/KB/CarExample.java
@@ -0,0 +1,9 @@
+package KB;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.setGas(0);
+        car.run();
+    }
+}
