6a9d039c5628417fc7e4b349de46272b597555f4
kdtTetz <xenosign@naver.com>
2024-06-22 19:07:26 +0900

    Update GasCar.java

--- src/extends1/GasCar.java
+++ src/extends1/GasCar.java
@@ -5,6 +5,10 @@ public class GasCar {
         System.out.println("가스차가 이동합니다");
     }
 
+    public void openDoor() {
+        System.out.println("차 문을 엽니다");
+    }
+
     public void fillUp() {
         System.out.println("가스차에 주유합니다");
     }
