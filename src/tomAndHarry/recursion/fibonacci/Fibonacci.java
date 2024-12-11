e888292846eb3fe18c2ad78a9787dd736aa58ed8
kdtTetz <xenosign@naver.com>
2024-12-11 23:10:49 +0900

    241211 재귀 횟수 추가

--- src/tomAndHarry/recursion/fibonacci/Fibonacci.java
+++ src/tomAndHarry/recursion/fibonacci/Fibonacci.java
@@ -2,14 +2,19 @@ package tomAndHarry.recursion.fibonacci;
 
 
 public class Fibonacci {
+    private static int cnt = 0;
+
     public int solution(int n) {
         if (n <= 1) return n;
+        cnt++;
 
         return solution(n - 1) + solution(n - 2);
     }
 
     public static void main(String[] args) {
         int num = 7;
+
         System.out.println(new Fibonacci().solution(num));
+        System.out.println("총 재귀 횟수 : " + cnt);
     }
 }
