74780212cbb07e32f1a113a849fcaf2bc4e06256
kdtTetz <xenosign@naver.com>
2024-12-11 23:08:01 +0900

    241211 재귀 설명용 피보나치 추가

---
+++ src/tomAndHarry/recursion/fibonacci/Fibonacci.java
@@ -0,0 +1,15 @@
+package tomAndHarry.recursion.fibonacci;
+
+
+public class Fibonacci {
+    public int solution(int n) {
+        if (n <= 1) return n;
+
+        return solution(n - 1) + solution(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int num = 7;
+        System.out.println(new Fibonacci().solution(num));
+    }
+}
