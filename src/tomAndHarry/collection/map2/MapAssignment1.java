060f84532d3676db4259f75761baf2171eff0d0c
xenosign <xenosign@naver.com>
2024-12-30 15:13:21 +0900

    250128 맥북 세탕

---
+++ src/tomAndHarry/collection/map2/MapAssignment1.java
@@ -0,0 +1,35 @@
+package tomAndHarry.collection.map2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapAssignment1 {
+    public static void main(String[] args) {
+        String[] tc1 = {"apple", "banana", "strawberry", "cherry", "grape", "apple", "banana", "apple"};
+        String[] tc2 = {"연비", "시완", "수민", "태웅", "규찬", "시완", "태웅"};
+        System.out.println(solution(tc1));
+        System.out.println(solution(tc2));
+    }
+
+
+    public static String solution(String[] strings) {
+        // HashMap을 사용하여 각 문자열의 빈도수 카운트
+        Map<String, Integer> frequencyMap = new HashMap<>();
+        for (String str : strings) {
+            frequencyMap.put(str, frequencyMap.getOrDefault(str, 0) + 1);
+        }
+
+        // 가장 높은 빈도수를 가진 문자열 찾기
+        String mostFrequent = null;
+        int maxFrequency = 0;
+
+        for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()) {
+            if (entry.getValue() > maxFrequency) {
+                maxFrequency = entry.getValue();
+                mostFrequent = entry.getKey();
+            }
+        }
+
+        return mostFrequent;
+    }
+}
